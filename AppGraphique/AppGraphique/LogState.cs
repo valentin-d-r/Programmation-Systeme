57e21634dd054f2c3c5d313fb95576b2cccd66fc
Valentin <valentin.dremaux@viacesi.fr>
2021-12-13 14:17:11 +0100

    Add MVVM architecture to the WPF project

---
+++ AppGraphique/AppGraphique/LogState.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Resources;
+using System.Reflection;
+using System.Threading;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AppGraphique.Model
+{
+    class LogState
+    {
+        string name;
+        string source;
+        string dest;
+        private DateTime timestamp;
+        private string state;
+
+        #region GETER AND SETER
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Source
+        {
+            get { return source; }
+            set { source = value; }
+        }
+        public string Dest
+        {
+            get { return dest; }
+            set { dest = value; }
+        }
+        public DateTime Timestamp
+        {
+            get { return timestamp; }
+            set { timestamp = value; }
+        }
+        #endregion
+
+        public void createLogState(LogState logState)
+        {
+
+            List<JSONStates> listJSON2 = new List<JSONStates>();
+            string json = JsonConvert.SerializeObject(logState);
+
+            Directory.CreateDirectory(@"C:\Backup\LogState");
+            File.AppendAllText(@"\Backup\LogState\LogState.json", json);
+
+            /*string fileName = @"\Backup\LogState\LogState.json";
+
+            if (!File.Exists(fileName))
+            {
+                listJSON2.Add(logState);
+                var options = new JsonSerializerOptions { WriteIndented = true };
+                string jsonString = System.Text.Json.JsonSerializer.Serialize(listJSON2, options);
+                File.WriteAllText(@fileName, jsonString);
+            }
+            else
+            {
+                StreamReader r = new StreamReader(fileName);
+                string jsonString2 = r.ReadToEnd();
+                r.Close();
+                List<JSONStates> m = JsonConvert.DeserializeObject<List<JSONStates>>(jsonString2);
+                m.Add(logState);
+                var options = new JsonSerializerOptions { WriteIndented = true };
+                string jsonString3 = System.Text.Json.JsonSerializer.Serialize(m, options);
+                File.WriteAllText(@fileName, jsonString3);
+            }*/
+        }
+    }
+}
---
+++ AppGraphique/AppGraphique/SaveModel.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Resources;
+using System.Reflection;
+using System.Threading;
+using System.Globalization;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using AppGraphique.Model;
+using System.Windows;
+using System.Diagnostics;
+
+namespace AppGraphique
+{
+    class SaveModel
+    {
+        private string name;
+        private string source;
+        private string dest;
+        private double size;
+        private DateTime timestamp;
+        private string fileTransferTime;
+        private string state;
+        private bool Copy = true;
+
+        #region GETER AND SETER
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public string Source
+        {
+            get { return source; }
+            set { source = value; }
+        }
+        public string Dest
+        {
+            get { return dest; }
+            set { dest = value; }
+        }
+        public double Size
+        {
+            get { return size; }
+            set { size = value; }
+        }
+        public DateTime Timestamp
+        {
+            get { return timestamp; }
+            set { timestamp = value; }
+        }
+        public string FileTransferTime
+        {
+            get { return fileTransferTime; }
+            set { fileTransferTime = value; }
+        }
+        public string State
+        {
+            get { return state; }
+            set { state = value; }
+        }
+        #endregion
+
+        public SaveModel()
+        {
+            Name = "";
+            Source = "";
+            Dest = "";
+            Size = 0;
+            Timestamp = default;
+            FileTransferTime = default;
+            State = "ACTIF";
+        }
+
+
+        public void createSave(Log logModel, LogState logStateModel)
+        {
+            State = "ACTIF";
+            DirectoryInfo dir = new DirectoryInfo(Source);
+            DirectoryInfo dest = new DirectoryInfo(Dest); DirectoryInfo[] dirs = dir.GetDirectories();
+            Directory.CreateDirectory(Dest);
+            FileInfo[] files = dir.GetFiles();
+            foreach (FileInfo file in files)
+            {
+                Console.WriteLine(file.Extension);
+                if (file.Extension == ".exe") // MERCI MAC QUI POUR .APP = .PLIST ????  AU FINAL CA FONCTIONNE PTDR
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.WriteLine("Présence d'un logiciel ! Sauvegarde INTERDITE");
+                    Console.ForegroundColor = ConsoleColor.Black;
+                    Copy = false;
+                    Directory.Delete(Dest, true);
+
+                    /*return Copy;*/
+                }
+                string tempPath = Path.Combine(Dest, file.Name);
+                file.CopyTo(tempPath, false);
+
+
+            }
+            // If copying subdirectories, copy them and their contents to new location.
+            if (copySubDirs)
+            {
+                foreach (DirectoryInfo subdir in dirs)
+                {
+                    string tempPath = Path.Combine(Dest, subdir.Name);
+                    createSave(copySubDirs);
+                }
+            }
+
+            /*return Copy;*/
+        }       
+    }
+}
